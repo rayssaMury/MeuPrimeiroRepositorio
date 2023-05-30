79da4a850cfc88c8ea7e20362ac7451a20d13639
Rayssa <rayssa.mury@aluno.cefet-rj.br>
2023-05-30 17:32:25 -0300

    arquivu

--- mains.c
+++ mains.c
@@ -2,17 +2,18 @@
 2 #include <stdio.h>
 3
 4 void imprimirBob(){
-printf("Bob");
+printf("Eu sou Bob!\n");
 5
-6
-7}
-8
-9 int main(){
-imprimirBob();
-10
-11 return 0;
+6}
+7
+8 void imprimirAlice(){
+9 printf("Eu sou Alice!\n");
 
-4 void imprimirBob(){
-printf("Bob");
+10 int main(){
+
+	imprimirBob();
+	
+	imprimirAlice();
+11 return 0;
 
 12}
